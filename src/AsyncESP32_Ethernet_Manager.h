9d426c29c71bb2ec4d8fba8aa61fef1544dde740
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2022-12-11 23:56:52 -0500

    v1.0.0 for `ESP32 + LwIP W5500 /  ENC28J60`
    #### Releases v1.0.0
    
    1. Initial coding to port [ESPAsync_WiFiManager](https://github.com/khoih-prog/ESPAsync_WiFiManager) to ESP32 boards using `LwIP W5500 / ENC28J60 Ethernet`.
    2. Use `allman astyle`

---
+++ src/AsyncESP32_Ethernet_Manager.h
@@ -0,0 +1,31 @@
+/****************************************************************************************************************************
+  AsyncESP32_Ethernet_Manager.h
+
+  For Ethernet shields using ESP32_Ethernet (ESP32 + LwIP W5500 / ENC28J60)
+
+  Modified from
+  1. Tzapu               (https://github.com/tzapu/WiFiManager)
+  2. Ken Taylor          (https://github.com/kentaylor)
+  3. Alan Steremberg     (https://github.com/alanswx/ESPAsyncWiFiManager)
+  4. Khoi Hoang          (https://github.com/khoih-prog/ESPAsync_WiFiManager)
+
+  Built by Khoi Hoang https://github.com/khoih-prog/AsyncESP32_Ethernet_Manager
+  Licensed under MIT license
+
+  Version: 1.0.0
+
+  Version Modified By  Date      Comments
+  ------- -----------  ---------- -----------
+  1.0.0   K Hoang     12/12/2022 Initial coding for (ESP32 + LwIP W5500 / ENC28J60)
+ *****************************************************************************************************************************/
+
+#pragma once
+
+#ifndef AsyncESP32_Ethernet_Manager_h
+#define AsyncESP32_Ethernet_Manager_h
+
+#include <AsyncESP32_Ethernet_Manager.hpp>              //https://github.com/khoih-prog/AsyncESP32_Ethernet_Manager
+#include <AsyncESP32_Ethernet_Manager_Impl.h>           //https://github.com/khoih-prog/AsyncESP32_Ethernet_Manager
+
+#endif    // AsyncESP32_Ethernet_Manager_h
+
